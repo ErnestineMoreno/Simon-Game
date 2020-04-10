0ff58bf48b4e81ce502fd34ed79dd53cbcc1b6b0
Vadertin-1104 <ernestinemoreno4@gmail.com>
2020-04-10 01:52:44 -0700

    HTML, basic JS, CSS foundation setup

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
 console.log('Come stai, Tina');
+
+//CONSTS
+const startButton = document.querySelector('.start-button');
+const restartButton = document.querySelector('.restart-button');
+const strictMode = document.querySelector('.strict-mode');
+const normalMode = document.querySelector('.normal-mode');
+// console.log(startButton);
+// console.log(restartButton);
+// console.log(strictMode);
+// console.log(normalMode);
+
+//EVENT LISTENERS
+startButton.addEventListener('click', startGame);
+restartButton.addEventListener('click', restartGame);
+strictMode.addEventListener('click', strictMode);
+normalMode.addEventListener('click', normalMode);
+
+//GAME LOGIC FUNCTIONS
+function startGame() {
+
+}
+
+
+function restartGame() {
+
+}
+
+
+function strictMode() {
+
+}
+
+
+
+function normalMode() {
+
+}
